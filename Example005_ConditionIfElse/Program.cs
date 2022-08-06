e6b05207aa3b0dc3f2b5e00588bb75e02f757d44
Hayk Khalatyan <hajkyli@mail.ru>
2022-08-06 14:45:18 +0300

    add Example 005 Condition If Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите пожалуйста имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username == "Гайк")
+{
+    Console.WriteLine("Ура, как я рад тебя видеть, дорогой Гайк!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
